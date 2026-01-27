d7f52e0789e28864c162d32040d688bb9ab9eef3
touseefx2 <x2app123@gmail.com>
2026-01-28 00:24:17 +0500

    Update index.tsx

--- app/(main)/dashboard/(explore)/index.tsx
+++ app/(main)/dashboard/(explore)/index.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import { useAppSelector } from "@/src/hooks/hooks";
 import NotificationsScreen from "../../notification";
 import ExploreScreen from "./Explore";
-import TryOnScreen from "./TryOnScreen";
 
 export default function Home() {
-
   const user = useAppSelector((state) => state.user);
   const userRole = user.userRole;
   const isGuest = user.isGuest;
   const isCustomer = userRole === "customer" || isGuest;
-
 
   if (isCustomer) {
     return <ExploreScreen />;
