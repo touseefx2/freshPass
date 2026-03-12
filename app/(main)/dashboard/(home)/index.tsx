0800cf86f6c20374288a3e2282cbd00f474ecf24
touseefx2 <x2app123@gmail.com>
2026-03-12 23:57:50 +0500

    done

--- app/(main)/dashboard/(home)/index.tsx
+++ app/(main)/dashboard/(home)/index.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useFocusEffect } from "expo-router";
+import * as Notifications from "expo-notifications";
 import { useAppSelector } from "@/src/hooks/hooks";
+import { handleNotificationPermission } from "@/src/services/notificationPermissionService";
 import HomeScreen from "@/src/components/dashboard/home";
 import HomeClientScreen from "@/src/components/dashboard/homeClient";
 
@@ -7,8 +10,18 @@ export default function Home() {
   const user = useAppSelector((state) => state.user);
   const userRole = user.userRole;
   const isGuest = user.isGuest;
-  
-  
+  const accessToken = user.accessToken;
+
+  useEffect(() => {
+    if (accessToken) {
+      getNotificationPermission();
+    }
+  }, []);
+
+  const getNotificationPermission = async () => {
+    const granted = await handleNotificationPermission();
+  };
+
   if (userRole === "customer" || isGuest) {
     return <HomeClientScreen />;
   }
